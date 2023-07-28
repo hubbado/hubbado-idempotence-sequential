da90273ee1186fc5cd9de25c30d74270786721bc
Alexander Repnikov <aleksander.repnikov@gmail.com>
2023-07-28 17:01:30 +0200

    sequences hash is duplicated when transform_write
    It will cause cloned instances to have own sequences hash object.
    It will allow create custom fixture to test sequencial idempotency.

--- lib/idempotence/sequential/controls/entity.rb
+++ lib/idempotence/sequential/controls/entity.rb
@@ -5,6 +5,12 @@ module Idempotence
         class SomeEntity
           include Schema::DataStructure
           include Idempotence::Sequential::EntitySequences
+
+          attr_reader :instance_transform_write_called
+
+          def transform_write(_data)
+            @instance_transform_write_called = true
+          end
         end
 
         def self.example
@@ -17,6 +23,16 @@ module Idempotence
 
         def self.id_increment
           5678
+        end
+
+        module WithSequences
+          def self.example
+            message = Controls::Message.example
+
+            entity = Entity.example
+            entity.record_sequence(message)
+            entity
+          end
         end
       end
     end
